816f857488663fd3894a0c4622dbbb090006ac3b
zxy <304246725@qq.com>
2014-11-22 22:42:16 +0800

    finish first time

--- jquery/js/changeImgs.js
+++ jquery/js/changeImgs.js
@@ -16,53 +16,35 @@ var img_move=(function(){
         this.target=target;
     };
     var changeImg_Default={
-        back:function(imgs,begainLeft){
-            var begainLeft=begainLeft;
-            var imgs=imgs;
-            var img_txt=imgs.next();
-            img_txt.animate({bottom:'-40px'});
+        change:function(target,array,n){
+            var imgs=target;
+            var imgs_left=array;
+//            console.log(imgs_left);
+            var len=imgs_left.length;
+//            console.log('n:'+n);
+            var num=(n<(len-1))?(n+1):0;
+//            console.log(num);
+            var endLeft=imgs_left[num];
+//            console.log('end:'+endLeft);
+            //获取说明栏
+            var img_p=imgs.parent().find('p');
+            var img_txt=img_p.parent().parent();
+            //获取说明栏高度
+            var height=img_txt.css('height');
+            //调整links颜色
+            var links=imgs.parent().find('li');
+            links.each(function(){
+                $(this).css({"background-color":"#ffffff"});
+            });
+            $(links[num]).css({"background-color":"#3e96e8"});
+            //隐藏说明栏
+            if(endLeft!==imgs.position().left)img_txt.animate({bottom:'-'+height});
+            //清除时间线上其他swich
             if(swich_now)clearTimeout(swich);
-            if(toBack_now)clearTimeout(toBack_now);
-            return function back_now(){
-                if(swich)clearTimeout(swich);
-                var nowLeft=imgs.position().left;//现在所处的位置
-                var backDistance=(begainLeft-nowLeft);//现在位置到终点的距离
-                // console.log(backDistance);
-                //缩短距离
-                if(backDistance>10){
-                    imgs.offset(function(index, current) {
-                        return {left:current.left+backDistance/10,top:current.top};
-                    })
-                }
-                else{
-                    var imgsPosition=imgs.offset();
-                    imgsPosition.left+=backDistance;
-                    imgs.offset(imgsPosition);
-                    img_txt.animate({bottom:'0px'});
-                }
-                // console.log(isChange);
-                //自调用
-//                console.log('toBack:'+toBack_now);
-                if(imgs.position().left!=begainLeft) {
-                    toBack_now=true;
-                    return toBack = setTimeout(back_now, 20);
-                }
-                toBack_now=false;
-            }
-
-
-        },
-        change:function(imgs,endLeft){
-            var imgs=imgs;
-            var endLeft=endLeft;
-            var img_txt=imgs.next();
-            img_txt.animate({bottom:'-40px'});
-            if(swich_now)clearTimeout(swich);
-            if(toBack_now)clearTimeout(toBack_now);
             return function change_now(){
                 var nowLeft=imgs.position().left;//现在所处的位置
                 var distance=(nowLeft-endLeft);//现在位置到终点的距离
-                // console.log('distance:'+distance);
+//                console.log('distance:'+distance);
                 //缩短距离
                 if (distance > 6||distance<-6) {
                     imgs.offset(function (index, current) {
@@ -73,6 +55,12 @@ var img_move=(function(){
                     var imgsPosition = imgs.offset();
                     imgsPosition.left -= distance;
                     imgs.offset(imgsPosition);
+                    //转换说明
+                    img_p.each(function(){
+                        $(this).css({'display':'none'});
+                    });
+                    $(img_p[num]).css({'display':'block'});
+                    //显示说明栏
                     img_txt.animate({bottom:'0px'});
                 }
 
@@ -81,147 +69,64 @@ var img_move=(function(){
 //                console.log('swich:'+swich_now);
                 if (imgs.position().left != endLeft) {
                     swich_now=true;
-                    return swich = setTimeout(change_now, 20);
+                    swich = setTimeout(change_now, 20);
                 }
-                swich_now=false;
+                else{
+                    swich_now=false;
+//                    console.log('num:'+num);
+                }
+                return num;
+
             }
 
         }
-
     };
     callMe.prototype.run=function(){
         swich_now=false;
-        toBack_now=false;
         var changeImg=this.changeImg;
         var target=this.target;
         var imgs;
         typeof target=="string"?imgs=document.getElementById(target):imgs=target;
         imgs=$(imgs);
-        var links=imgs.parent().find('li');
+
         var chaImg=imgs.find('img');
         var imgWith=chaImg.width();         //图片宽度
 //        var begainLeft=imgs.offset().left;	//获取最初的位置
         var begainLeft=imgs.position().left;//由offset取得的是图片左侧距离浏览器左侧的距离
 //        parseInt(imgs.css('left'))效果同上
-//        var links_begain=begainLeft+imgWith;//links_left起点
-        var links_left=[];
-        links_left[0]=begainLeft;
-        var len=links.length;
+        //取每个图片的left值
+        var imgs_left=[];
+        imgs_left[0]=begainLeft;
+        var len =chaImg.length;
         for(var i= 0;i<len-1;i++){
-            links_left[i+1]=links_left[i]-imgWith;
+            imgs_left[i+1]=imgs_left[i]-imgWith;
         }
-        var back=imgs.width()-imgWith;
-//        console.log(begainLeft);
-//        console.log(back);
+        var num=0;
+//        console.log(imgs_left);
+        //调用change函数开始切换图片
         function moveStart(){
-
-            var endLeft=imgs.position().left-imgWith;//获取最终要到达的位置
-            // console.log('begain:'+begainLeft+';end:'+endLeft);
-            //拉回起点
-            if(begainLeft-imgs.position().left>=back)changeImg.back(imgs,begainLeft)();
-            //切换一个图片
-            else{changeImg.change(imgs,endLeft)()}
+            num=changeImg.change(imgs,imgs_left,num)();
+//            console.log('numsssss:'+num);
         }
         var changeStart=setInterval(moveStart,6000);
-        setInterval(function(){
-            var now_left=imgs.position().left;
-            var img_p=imgs.next().find('p');
-            switch(now_left){
-                case links_left[0]:
-                    links.each(function(){
-                        $(this).css({"background-color":"#ffffff"});
-                    });
-                    $(links[0]).css({"background-color":"#3e96e8"});
-                    img_p.each(function(){
-                        $(this).css({'display':'none'});
-                    });
-                    $(img_p[0]).css({'display':'block'});
-                    break;
-                case links_left[1]:
-                    links.each(function(){
-                        $(this).css({"background-color":"#ffffff"});
-                    });
-                    $(links[1]).css({"background-color":"#3e96e8"});
-                    img_p.each(function(){
-                        $(this).css({'display':'none'});
-                    });
-                    $(img_p[1]).css({'display':'block'});
-                    break;
-                case links_left[2]:
-                    links.each(function(){
-                        $(this).css({"background-color":"#ffffff"});
-                    });
-                    $(links[2]).css({"background-color":"#3e96e8"});
-                    img_p.each(function(){
-                        $(this).css({'display':'none'});
-                    });
-                    $(img_p[2]).css({'display':'block'});
-                    break;
-                case links_left[3]:
-                    links.each(function(){
-                        $(this).css({"background-color":"#ffffff"});
-                    });
-                    $(links[3]).css({"background-color":"#3e96e8"});
-                    img_p.each(function(){
-                        $(this).css({'display':'none'});
-                    });
-                    $(img_p[3]).css({'display':'block'});
-                    break;
-                case links_left[4]:
-                    links.each(function(){
-                        $(this).css({"background-color":"#ffffff"});
-                    });
-                    $(links[4]).css({"background-color":"#3e96e8"});
-                    img_p.each(function(){
-                        $(this).css({'display':'none'});
-                    });
-                    $(img_p[4]).css({'display':'block'});
-                    break;
-            }
-        },100);
-        var links_1=links_left[0];
-        var links_2=links_left[1];
-        var links_3=links_left[2];
-        var links_4=links_left[3];
-        var links_5=links_left[4];
-        $(links[0]).bind('click',function(){
-            if(changeStart)clearInterval(changeStart);
-            changeImg.change(imgs,links_1)();
-            changeStart=setInterval(moveStart,4000);
-        });
-        $(links[1]).bind('click',function(){
-            if(changeStart)clearInterval(changeStart);
-            changeImg.change(imgs,links_2)();
-            changeStart=setInterval(moveStart,4000);
-        });
-        $(links[2]).bind('click',function(){
-            if(changeStart)clearInterval(changeStart);
-            changeImg.change(imgs,links_3)();
-            changeStart=setInterval(moveStart,4000);
-        });
-        $(links[3]).bind('click',function(){
-            if(changeStart)clearInterval(changeStart);
-            changeImg.change(imgs,links_4)();
-            changeStart=setInterval(moveStart,4000);
-        });
-        $(links[4]).bind('click',function(){
-            if(changeStart)clearInterval(changeStart);
-            changeImg.change(imgs,links_5)();
-            changeStart=setInterval(moveStart,4000);
-        });
+        var links=imgs.parent().find('li');
+        $(links[0]).css({"background-color":"#3e96e8"});
+        //每个link加上序号
+        for(var j=0;j<len;j++){
+            $(links[j]).attr({'n':j});
+        }
+        //加上点击效果
+        links.bind('click',function(){
+             if(changeStart)clearInterval(changeStart);
+             var n=parseInt($(this).attr('n'))-1;
+             num=changeImg.change(imgs,imgs_left,n)();
+             changeStart=setInterval(moveStart,4000);
+         });
         setInterval(function(){
             if(imgs.offset().left<(begainLeft-4*imgWith))changeImg.back(imgs,begainLeft)();
         },100);
-//        for(var j=0;j<len;j++){
-//            $(links[j]).bind('click',function(){
-//                if(changeStart)clearTimeout(changeStart);
-//                changeImg.change(imgs,links_left[j])();
-//                changeStart=setInterval(moveStart,4000);
-//            });
-//
-//            alert(links_left[j]);
-//
-//        }
     };
     return callMe;
 })();
+var a= new img_move({},allImgs);
+a.run();
